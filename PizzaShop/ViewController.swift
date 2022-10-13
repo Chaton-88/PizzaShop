278ca26905e0321e47b0240e8250c4959462d04c
Valeriya Trofimova <chaton-2410@mail.ru>
2022-10-13 19:36:47 +0300

    Initial Commit

---
+++ PizzaShop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PizzaShop
+//
+//  Created by Valeriya Trofimova on 13.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
